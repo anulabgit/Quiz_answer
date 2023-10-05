7d2afd9b8012b23b67e994f98b33708126e9ad73
anulabgit <127391777+anulabgit@users.noreply.github.com>
2023-10-05 15:15:28 +0900

    Add files via upload

--- Quiz_answer.js
+++ Quiz_answer.js
@@ -1,21 +1,29 @@
 let img; // 회전시킬 이미지
 let angle = 12; // 회전 각도
-let lim= 180; // 이진화 임계값 설정
+let limit = 180; // 이진화 임계값 설정
 function preload() {
   img = loadImage('test.png');
 }
 
 function setup() {
+  slider_limit  = createSlider(0, 255, 180);// 이진화 임계값 설정
+  slider_limit.position(10, 10);
+  slider_limit.style('width', '80px');
+  slider_angle = createSlider(0, 255, 12);// 회전 각도 설정
+  slider_angle.position(10, 30);
+  slider_angle.style('width', '80px');
   createCanvas(500, 500);
   imageMode(CENTER);
 }
 
 function draw() {
+  let angle = slider_angle.value();
   translate(width / 2, height / 2); // 회전 중심을 캔버스 중앙으로 이동
   rotateImage(img, angle);
 }
 
 function rotateImage(img, a) {
+  let limit = slider_limit.value();
   // 수학적 회전 공식을 적용하기 위해 행렬 변환 사용
   let c = cos(radians(a));
   let s = sin(radians(a));
@@ -32,7 +40,7 @@ function rotateImage(img, a) {
 
     // 픽셀 값을 그레이스케일로 변환
     let gray = (r + g + b) / 3;
-    let threshold = lim;
+    let threshold = limit;
 
     // 이진화
     if (gray > threshold) {
